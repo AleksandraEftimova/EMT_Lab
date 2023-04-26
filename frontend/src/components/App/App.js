a13bbf4c3ba5e2cb7fe71e3aefdba2ffbc8673df
Aleksandra <aleftimova@gmail.com>
2023-04-26 13:15:49 +0200

    EMT - Lab2

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            authors: []
+        }
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default App;
